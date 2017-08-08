c29cc13ae76fadb6f6ac35b43cf49338c4afec04
Micael Robles <micael.robles@gmail.com>
2017-08-08 14:54:04 -0300

    pretty much done

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,59 @@ import Head from 'next/head'
 const logo = <img src="/static/logo.png" alt="" />
 const cookie = <img src="/static/cookie.png" alt="" />
 
+class Button extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    let label = this.props.tab === 'recent' ? 'THIS MONTH' : 'ALL TIME'
+    let classes = 'button'
+    if (this.props.selected) {
+      classes += ' selected'
+    }
+    return (
+      <div
+        className={classes}
+        onClick={() => {
+          this.props.switchTab(this.props.tab)
+        }}
+      >
+        <p>
+          {label}
+        </p>
+        <style jsx>{`
+          .button {
+            background: #6b2737;
+            width: 140px;
+            height: 28.97px;
+            padding: 5px;
+            box-sizing: border-box;
+            cursor: pointer;
+            transition: 0.14s ease all;
+          }
+          .button:hover {
+            background: #8d252e;
+          }
+          .selected {
+            background: #f45b69;
+          }
+          .selected:hover {
+            background: #f45b69 !important;
+          }
+          .button p {
+            font-family: OpenSans-SemiBold;
+            font-size: 12.87px;
+            color: #ffffff;
+            letter-spacing: 0.03px;
+            margin: 0;
+            text-align: center;
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
+
 class UserCard extends React.Component {
   constructor(props) {
     super(props)
@@ -28,6 +81,7 @@ class UserCard extends React.Component {
         <style jsx>
           {`
             .user {
+              margin-bottom: 9px;
               display: flex;
               width: 293.33px;
               height: 66.21px;
@@ -75,11 +129,13 @@ class UserCard extends React.Component {
 class App extends React.Component {
   constructor(props) {
     super(props)
+    this.switchTab = this.switchTab.bind(this)
     this.state = {
       tab: 'recent',
     }
   }
 
+  // API Call
   static async getInitialProps({ req }) {
     const resRecent = await fetch(
       'https://fcctop100.herokuapp.com/api/fccusers/top/recent'
@@ -93,6 +149,10 @@ class App extends React.Component {
       recent: jsonRecent,
       alltime: jsonAllTime,
     }
+  }
+
+  switchTab(des) {
+    this.setState(prevState => ({ tab: des }))
   }
 
   render() {
@@ -109,16 +169,70 @@ class App extends React.Component {
             href="https://fonts.googleapis.com/css?family=Open+Sans:300,600"
             rel="stylesheet"
           />
-        </Head>
-        {logo}
-        {userArray.map(user =>
-          <UserCard
-            key={user.username}
-            username={user.username}
-            img={user.img}
-            cookies={user[this.state.tab]}
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css"
           />
-        )}
+        </Head>
+        <div className="flex-center">
+          <div className="container">
+            {logo}
+            <div className="tabs">
+              <Button
+                tab="recent"
+                selected={this.state.tab === 'recent'}
+                switchTab={this.switchTab}
+              />
+              <Button
+                tab="alltime"
+                selected={this.state.tab === 'alltime'}
+                switchTab={this.switchTab}
+              />
+            </div>
+            <div className="leaderboard">
+              {userArray.map(user =>
+                <UserCard
+                  key={user.username}
+                  username={user.username}
+                  img={user.img}
+                  cookies={user[this.state.tab]}
+                />
+              )}
+            </div>
+          </div>
+        </div>
+        <style jsx>
+          {`
+            .flex-center {
+              width: 100vw;
+              height: 100vh;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              background-color: black;
+            }
+            .container {
+              background: #114b5f;
+              width: 320px;
+              height: 409px;
+              padding: 13.1px;
+              box-sizing: border-box;
+            }
+            .leaderboard {
+              height: 260px;
+              overflow-y: scroll;
+            }
+            .tabs {
+              justify-content: space-between;
+              display: flex;
+              margin: 10px 0;
+            }
+            ::-webkit-scrollbar {
+              display: none;
+            }
+          `}
+        </style>
       </div>
     )
   }
